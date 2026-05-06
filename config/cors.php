3f37d26d65b34d5b1e21cd474134f0b6777cc6a2
syad <irsyadysfb05@gmail.com>
2026-05-06 14:28:43 +0800

    update cors

---
+++ config/cors.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+
+    'allowed_methods' => ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+
+    'allowed_origins' => [
+        env('FRONTEND_URL', 'https://sio.mysztechnology.com'),
+    ],
+
+    'allowed_origins_patterns' => [],
+
+    'allowed_headers' => ['*'],
+
+    'exposed_headers' => [],
+
+    'max_age' => 0,
+
+    'supports_credentials' => true,
+
+];
